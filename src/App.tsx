6eef614a2c9518a4b8244d7dbf614d1df3b61c23
CodeSandbox <support@codesandbox.io>
2023-10-23 15:59:09 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,96 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+//Nome: Florbela Freitas Oliveira | RM: 99475
+
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [joke, setJoke] = useState([]);
+  const [favorite, setFavorite] = useState(false);
+  const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const list = localStorage.getItem("FavoriteList");
+    const JSONList = JSON.parse(list) || [];
+
+    setFavorites(JSONList);
+  });
+
+  useEffect(() => {
+    console.log("Getting joke...");
+    getJoke();
+  }, []);
+
+  const getJoke = () => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+
+      .then((data) => {
+        console.log(data);
+        setJoke(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  };
+
+  const handleJoke = (joke) => {
+    const newJoke = [...favorites];
+    newJoke.push(joke);
+
+    const stringList = JSON.stringify(newJoke);
+    localStorage.setItem("FavoriteList", stringList);
+
+    setFavorites(newJoke);
+    setFavorite(true);
+  };
+
+  const deleteFavJoke = (favJoke) => {
+    if (window.confirm("Tem certeza que deseja deletar?")) {
+      const newFavorites = favorites.filter((joke) => joke.id !== favJoke.id);
+      const stringList = JSON.stringify(newFavorites);
+      localStorage.setItem("FavoriteList", stringList);
+      setFavorites(newFavorites);
+    }
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
+      <h1>Chuck Norris</h1>
 
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
+      <div className="Container">
+        <div className="JokeContainer">
+          <p key={joke.id} className="Joke">
+            {joke.value}
+          </p>
+        </div>
+
+        <div className="Favorite" onClick={() => handleJoke(joke)}>
+          <img
+            className="img"
+            src={
+              favorite
+                ? "src/assets/favorito.png"
+                : "src/assets/adicionar-aos-favoritos.png"
+            }
+            alt={favorite ? "Desfavoritar" : "Favoritar"}
+          />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>Lista de favoritos</h1>
+
+      {favorites.map((favorite) => (
+        <ul>
+          <li key={favorite.id}>{favorite.value}</li>
+
+          <div className="Favorite" onClick={() => deleteFavJoke(favorite)}>
+            <img className="img" src="src/assets/excluir.png" alt="Excluir" />
+          </div>
+        </ul>
+      ))}
     </div>
   );
 }
